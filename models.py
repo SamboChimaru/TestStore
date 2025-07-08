275716e35d1cb99e81cd7b23bc6e792a710111fc
SamboChimaru <kuoysambo@gmail.com>
2025-07-08 14:18:38 +0700

    models.py

---
+++ models.py
@@ -0,0 +1,11 @@
+from sqlalchemy import Column, Integer, String, DateTime
+from sqlalchemy.orm import declarative_base
+from datetime import datetime
+
+Base = declarative_base()
+
+class User(Base):
+    __tablename__ = "users"
+    id = Column(Integer, primary_key=True, index=True)
+    username = Column(String, unique=True, index=True)
+    joined_at = Column(DateTime, default=datetime.utcnow)
